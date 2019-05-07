f5763ab65b438f79fa7a8259c234b47a4fb13dde
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2019-05-07 09:21:23 +0100

    commander

--- i2e.js
+++ i2e.js
@@ -2,35 +2,47 @@
 const fs = require("fs");
 let insp = require("util").inspect;
 let ins = (x) => insp(x, {depth:null});
+const spawn = require('child_process').spawn;
 const PEG = require("./infix2egg.js");
-let version = require('package.json').version;
+let package = require('package.json');
+let version = package.version;
+let description = package.description;
+let evm = package.evm;
 
-let program = require('commander');
+let commander = require('commander');
  
 
 
-program
+commander
   .version(version)
+  .description(description)
   .usage('[options] <programPath>')
   .option('-t, --tree', 'Show AST')
   .option('-o, --output [file]', 'Output file')
+  .option('-r, --run', 'Run program')
   .parse(process.argv);
 
 try {
-  if (!program.args.length) program.help();
+  if (!commander.args.length) commander.help();
 
-  let fileName = program.args.shift();
+  let fileName = commander.args.shift();
 
   let infixProgram = fs.readFileSync(fileName, 'utf8');
   let tree = PEG.parse(infixProgram);
   let json = JSON.stringify(tree, null, "  ");
-  if (program.tree) console.log(`program: ${infixProgram}\nresult = ${json}`);
-  let outputFile = (program.output && program.output) || fileName+".evm";
+  if (commander.tree) console.log(`program: ${infixProgram}\nresult = ${json}`);
+  let outputFile = (commander.output && commander.output) || fileName+".evm";
   fs.writeFileSync(outputFile, json);
+  if (commander.run) {
+    const evmRun = spawn(evm, [ outputFile ]);
+    evmRun.stdout.pipe(process.stdout);
+  }
 }
 catch (err) {
-  console.log(err);
-  console.log(`${err.message} at line ${err.location.start.line} column ${err.location.start.column} offset ${err.location.start.offset}`);
+  if (err.message && err.location)
+    console.log(`${err.message} at line ${err.location.start.line} column ${err.location.start.column} offset ${err.location.start.offset}`);
+  else console.log(err);
+  commander.help();
 }
 
 
